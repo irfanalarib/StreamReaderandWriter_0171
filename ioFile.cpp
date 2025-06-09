2e963553705253d054b307dc3a66e39dfc2ba358
irfanalarib <irfanalarib7@gmail.com>
2025-06-09 14:34:03 +0700

    add kondisi keluar dari loop

--- ioFile.cpp
+++ ioFile.cpp
@@ -18,5 +18,7 @@ int main() {
         cout << "_ ";
         //mendapatkan setiap karakter dalam satu baris
         getline(cin, baris);
+        // loop akan berhenti jika anda memasukan karakter q
+        if (baris == "q") break;
     }
 }
