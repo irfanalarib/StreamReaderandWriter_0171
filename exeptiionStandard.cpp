55bde391a62de4806a605e6e238a8a6584770fd2
irfanalarib <irfanalarib7@gmail.com>
2025-06-09 15:28:27 +0700

    Menambahkan fungsi main dengan blok try untuk  elemen array

--- exeptiionStandard.cpp
+++ exeptiionStandard.cpp
@@ -4,3 +4,13 @@
 #include <array>
 //untuk objek array yang akan digunakan
 using namespace std;
+
+int main()
+{
+    cout << "Awal Program" << endl; //penanda 1:...
+    try {
+        array<int, 3> data = {10, 20, 30 };//pesan array integer 3 elemen
+        cout<<data.at(5)<< endl;
+        //memanggil array elemen ke 5
+    }
+}
