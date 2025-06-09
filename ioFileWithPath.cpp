c854da477455682053c0d14f87091a71dcb8cbf7
irfanalarib <irfanalarib7@gmail.com>
2025-06-09 15:11:48 +0700

    mengubah format file menjadi csv

---
+++ ioFileWithPath.cpp
@@ -0,0 +1,55 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string baris;
+    string namefile;
+
+    cout << "Memasukan Name File : ";
+    cin >> namefile;
+    cin.ignore(); 
+
+    //membuat ojek output file stram
+    ofstream outfile;
+    //membuka file untuk ditulisi
+    outfile.open(namefile + ".csv");
+
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+    //unlimited lopp untuk menulis
+    while (true) {
+        cout << "- ";
+        //mendapatkan setiap karakter dalam satu baris
+        getline(cin, baris);
+        // loop akan berhenti jika anda memasukan karakter q
+        if (baris == "q") break;
+        //menulis dan memasukan nilai dari 'baris' ke dalam file
+        outfile << baris << endl;
+    }
+    //selesai dalam menulis sekarang tutup filenya
+    outfile.close();
+
+    //membuat objek input file stram
+    ifstream infile;
+    //membuka file yang dituliskan
+    infile.open(namefile + ".csv");
+
+    cout << endl << ">= Membuka dan membaca file " << endl;
+    //jika file ada maka
+    if (infile.is_open())
+    {
+        //melakukan perulangan setiap baris
+        while (getline(infile, baris))
+        {
+            //dan tampilkan disini
+            cout << baris << '\n';
+        }
+        //tutup file tersebut setelah selesai
+        infile.close();
+    }
+    //jika tidak ditemukan file makan akan menampilkan ini
+    else cout << "Unable to open file";
+    return 0;
+}
