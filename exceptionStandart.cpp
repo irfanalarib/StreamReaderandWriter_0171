e9b6cbd988ae28a17f00de71d553390c0dfb0924
irfanalarib <irfanalarib7@gmail.com>
2026-06-04 16:41:14 +0700

    memanggil array dengan index 5

--- exceptionStandart.cpp
+++ exceptionStandart.cpp
@@ -9,5 +9,7 @@ int main()
     try {
         // deklarasi array dengan panjang 3
         array<int, 3> data = {1, 2, 3};
+        //memanggil array dengan index 5 yang berarti akan terjadi error
+        cout << data.at(5) << endl;
     }
 }
