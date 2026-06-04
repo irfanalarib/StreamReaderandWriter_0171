37a0b46f7e02dc77e0fba38e84ff5fc9175d5e6f
irfanalarib <irfanalarib7@gmail.com>
2026-06-04 16:45:25 +0700

    membuat isi dari try

--- manualExcaption.cpp
+++ manualExcaption.cpp
@@ -1,2 +1,12 @@
 #include <iostream>
 using namespace std;
+
+int main(){
+
+    //membuat isi dari try untuk menguji error
+    try {
+        cout << "Selamat Belajar Di Prodi TI UMY" << endl;
+        //throw 0.5; //melemparkan sebuah integer main
+        cout << "Pernyataan tidak akan di tampilkan" << endl;
+    }
+}
