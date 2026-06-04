d5aa8c3ee9fdf6e12f3032e03b62737402ded842
irfanalarib <irfanalarib7@gmail.com>
2026-06-04 16:22:14 +0700

    menambahkan inputan untuk nama file

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -6,4 +6,8 @@ using namespace std;
 int main(){
     //deklarasi variabel baris dan namafile
     string baris, NamaFile;
+
+    //membuat inputan nama file
+    cout << "Masukkan nama file: ";
+    cin >> NamaFile;
 }
