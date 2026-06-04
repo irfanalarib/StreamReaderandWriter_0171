42bc3ef1b72c6729889b4665e116289e5b9f76c7
irfanalarib <irfanalarib7@gmail.com>
2026-06-04 16:40:54 +0700

    deklarasi array

--- exceptionStandart.cpp
+++ exceptionStandart.cpp
@@ -2,3 +2,12 @@
 #include <exception>
 #include <array>
 using namespace std;
+
+int main() 
+{
+    cout << "Awal program" << endl; //penanda
+    try {
+        // deklarasi array dengan panjang 3
+        array<int, 3> data = {1, 2, 3};
+    }
+}
